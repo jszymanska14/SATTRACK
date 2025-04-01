9142cd27d6d795b8d4cd2d02b4f76a84cec9049b
Zuzanna Slobodzian <zuzanna.slobodzian@gmail.com>
2025-04-01 10:39:32 +0200

    urls set up

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sat_track'
 ]
 
 MIDDLEWARE = [
@@ -54,8 +55,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'sat_track' / 'templates']
+
+    ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
